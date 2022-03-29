2e254251fcd96ec11f09c907931af4bc8d064cd9
RafaelManfrim <rafaelmanfrim2004@gmail.com>
2022-03-29 19:37:56 -0300

    visual do código

--- lib/report_generator.ex
+++ lib/report_generator.ex
@@ -1,7 +1,5 @@
 defmodule ReportGenerator do
-  def build(filename) do
-    "report_files/#{filename}" |> File.read() |> handle_file()
-  end
+  def build(filename), do: "report_files/#{filename}" |> File.read() |> handle_file()
 
   defp handle_file({:ok, file_content}), do: file_content
   defp handle_file({:error, _reason}), do: "Error while opening file!"

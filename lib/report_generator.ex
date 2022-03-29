92554e37ed048e8cb63b9ef696c6cdea834e5261
RafaelManfrim <rafaelmanfrim2004@gmail.com>
2022-03-29 19:51:33 -0300

    parseando csv em lista

--- lib/report_generator.ex
+++ lib/report_generator.ex
@@ -1,6 +1,8 @@
 defmodule ReportGenerator do
-  def build(filename), do: "report_files/#{filename}" |> File.read() |> handle_file()
+  def build(filename),
+    do: "report_files/#{filename}" |> File.stream!() |> Enum.map(fn line -> parse_line(line) end)
 
-  defp handle_file({:ok, file_content}), do: file_content
-  defp handle_file({:error, _reason}), do: "Error while opening file!"
+  defp parse_line(line) do
+    line |> String.trim() |> String.split(",") |> List.update_at(2, &String.to_integer/1)
+  end
 end

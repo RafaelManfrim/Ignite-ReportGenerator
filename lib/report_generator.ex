ec0a08ab92f028e1fb35ae88a0c20b94297bbfd0
RafaelManfrim <rafaelmanfrim2004@gmail.com>
2022-03-30 20:49:33 -0300

    funcionalidade fetch_higher_cost

--- lib/report_generator.ex
+++ lib/report_generator.ex
@@ -6,6 +6,8 @@ defmodule ReportGenerator do
     |> Enum.reduce(report_acc(), fn line, report -> sum_values(line, report) end)
   end
 
+  def fetch_higher_cost(report), do: Enum.max_by(report, fn {_key, value} -> value end)
+
   defp sum_values([id, _food_name, price], report), do: Map.put(report, id, report[id] + price)
 
   defp report_acc, do: Enum.into(1..30, %{}, fn x -> {Integer.to_string(x), 0} end)

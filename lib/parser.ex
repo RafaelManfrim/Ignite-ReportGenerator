fe70033d3ea772fc32ebda4d669f09e9aeeb71fe
RafaelManfrim <rafaelmanfrim2004@gmail.com>
2022-03-30 20:43:21 -0300

    Criando modulo Parser e função sum_values

---
+++ lib/parser.ex
@@ -0,0 +1,11 @@
+defmodule ReportGenerator.Parser do
+  def parse_file(filename) do
+    "report_files/#{filename}"
+    |> File.stream!()
+    |> Stream.map(fn line -> parse_line(line) end)
+  end
+
+  defp parse_line(line) do
+    line |> String.trim() |> String.split(",") |> List.update_at(2, &String.to_integer/1)
+  end
+end
--- lib/report_generator.ex
+++ lib/report_generator.ex
@@ -1,16 +1,12 @@
 defmodule ReportGenerator do
+  alias ReportGenerator.Parser
+
   def build(filename) do
-    "report_files/#{filename}"
-    |> File.stream!()
-    |> Enum.reduce(report_acc(), fn line, report ->
-      [id, _food_name, price] = parse_line(line)
-      Map.put(report, id, report[id] + price)
-    end)
+    Parser.parse_file(filename)
+    |> Enum.reduce(report_acc(), fn line, report -> sum_values(line, report) end)
   end
 
-  defp parse_line(line) do
-    line |> String.trim() |> String.split(",") |> List.update_at(2, &String.to_integer/1)
-  end
+  defp sum_values([id, _food_name, price], report), do: Map.put(report, id, report[id] + price)
 
   defp report_acc, do: Enum.into(1..30, %{}, fn x -> {Integer.to_string(x), 0} end)
 end

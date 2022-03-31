ac6d4f9d0f7e1bb7cfd6174559dbe233e01515c4
RafaelManfrim <rafaelmanfrim2004@gmail.com>
2022-03-30 23:50:08 -0300

    refatorando sum_values e report_acc adicionando contador por alimento

--- lib/report_generator.ex
+++ lib/report_generator.ex
@@ -1,6 +1,17 @@
 defmodule ReportGenerator do
   alias ReportGenerator.Parser
 
+  @available_foods [
+    "açaí",
+    "churrasco",
+    "esfirra",
+    "hambúrger",
+    "pastel",
+    "pizza",
+    "prato_feito",
+    "sushi"
+  ]
+
   def build(filename) do
     Parser.parse_file(filename)
     |> Enum.reduce(report_acc(), fn line, report -> sum_values(line, report) end)
@@ -8,7 +19,17 @@ defmodule ReportGenerator do
 
   def fetch_higher_cost(report), do: Enum.max_by(report, fn {_key, value} -> value end)
 
-  defp sum_values([id, _food_name, price], report), do: Map.put(report, id, report[id] + price)
+  defp sum_values([id, food_name, price], %{"users" => users, "foods" => foods} = report) do
+    users = Map.put(users, id, users[id] + price)
+    foods = Map.put(foods, food_name, foods[food_name] + 1)
 
-  defp report_acc, do: Enum.into(1..30, %{}, fn x -> {Integer.to_string(x), 0} end)
+    %{report | "users" => users, "foods" => foods}
+  end
+
+  defp report_acc do
+    users = Enum.into(1..30, %{}, fn x -> {x, 0} end)
+    foods = Enum.into(@available_foods, %{}, fn x -> {Integer.to_string(x), 0} end)
+
+    %{"users" => users, "foods" => foods}
+  end
 end

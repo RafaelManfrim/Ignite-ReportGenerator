086460c019c9ec622a96be09323b1a2e6a8e210a
RafaelManfrim <rafaelmanfrim2004@gmail.com>
2022-04-08 18:26:01 -0300

    Testando a versão paralela

--- test/report_generator_test.exs
+++ test/report_generator_test.exs
@@ -56,6 +56,73 @@ defmodule ReportGeneratorTest do
     end
   end
 
+  describe "build_from_many/1" do
+    test "when a file list provided, builds the report" do
+      filenames = ["report_1.csv", "report_2.csv", "report_3.csv"]
+
+      response = ReportGenerator.build_from_many(filenames)
+
+      expect_response =
+        {:ok,
+         %{
+           "foods" => %{
+             "açaí" => 37_742,
+             "churrasco" => 37_650,
+             "esfirra" => 37_462,
+             "hambúrguer" => 37_577,
+             "pastel" => 37_392,
+             "pizza" => 37_365,
+             "prato_feito" => 37_519,
+             "sushi" => 37_293
+           },
+           "users" => %{
+             "1" => 278_849,
+             "10" => 268_317,
+             "11" => 268_877,
+             "12" => 276_306,
+             "13" => 282_953,
+             "14" => 277_084,
+             "15" => 280_105,
+             "16" => 271_831,
+             "17" => 272_883,
+             "18" => 271_421,
+             "19" => 277_720,
+             "2" => 271_031,
+             "20" => 273_446,
+             "21" => 275_026,
+             "22" => 278_025,
+             "23" => 276_523,
+             "24" => 274_481,
+             "25" => 274_512,
+             "26" => 274_199,
+             "27" => 278_001,
+             "28" => 274_256,
+             "29" => 273_030,
+             "3" => 272_250,
+             "30" => 275_978,
+             "4" => 277_054,
+             "5" => 270_926,
+             "6" => 272_053,
+             "7" => 273_112,
+             "8" => 275_161,
+             "9" => 274_003
+           }
+         }}
+
+      assert response == expect_response
+    end
+
+    test "when a file list is not provided, return error" do
+      filenames = "banana"
+
+      response = ReportGenerator.build_from_many(filenames)
+
+      expect_response = {:error, "Please provide a list of strings"}
+
+      assert response == expect_response
+    end
+  end
+
   describe "fetch_higher_cost/2" do
     test "when the option is 'users', returns the user who spent the most" do
       file_name = "report_test.csv"
